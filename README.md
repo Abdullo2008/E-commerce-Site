6f03b7dacc98f7d72068f838123afae040986c0e
Abdullo2008 <igamberdiyevabdullo754@gmail.com>
2024-08-12 00:24:00 +0500

    hiding secret key, creating gitignore and README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+Creating virtualenv (in terminal):
+pip install pipenv
+then 
+pipenv shell
+then
+pipenv install django
--- kassa/settings.py
+++ kassa/settings.py
@@ -9,18 +9,20 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import os.path
+import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+import dj_database_url
+from environs import Env
 
+env = Env()
+env.read_env()
 
+SECRET_KEY = env.str("SECRET_KEY")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a)0umr-@8ida3=#e8$3073fx@$fz^sc=gdx)*q=+dicl96+u5='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
